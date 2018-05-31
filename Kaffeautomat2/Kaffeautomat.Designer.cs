1bd6bc3541ca95fa281966a27b1044bd9c831be1
ShermanWierdo <ealean@edu.ronneby.se>
2018-05-31 11:00:13 +0200

    blargh

---
+++ Kaffeautomat2/Kaffeautomat.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Kaffeautomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnTio = new System.Windows.Forms.Button();
+            this.btnFem = new System.Windows.Forms.Button();
+            this.btnEn = new System.Windows.Forms.Button();
+            this.btnFemtioore = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVaxel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnMocka = new System.Windows.Forms.Button();
+            this.btnChocklad = new System.Windows.Forms.Button();
+            this.btnExpresso = new System.Windows.Forms.Button();
+            this.btnNyponsoppa = new System.Windows.Forms.Button();
+            this.btnFruktsoppa = new System.Windows.Forms.Button();
+            this.btnMotorolja = new System.Windows.Forms.Button();
+            this.btnKaffe = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTio
+            // 
+            this.btnTio.Location = new System.Drawing.Point(12, 47);
+            this.btnTio.Name = "btnTio";
+            this.btnTio.Size = new System.Drawing.Size(75, 23);
+            this.btnTio.TabIndex = 0;
+            this.btnTio.Text = "10:-";
+            this.btnTio.UseVisualStyleBackColor = true;
+            this.btnTio.Click += new System.EventHandler(this.btnTio_Click);
+            // 
+            // btnFem
+            // 
+            this.btnFem.Location = new System.Drawing.Point(12, 76);
+            this.btnFem.Name = "btnFem";
+            this.btnFem.Size = new System.Drawing.Size(75, 23);
+            this.btnFem.TabIndex = 1;
+            this.btnFem.Text = "5:-";
+            this.btnFem.UseVisualStyleBackColor = true;
+            this.btnFem.Click += new System.EventHandler(this.btnFem_Click);
+            // 
+            // btnEn
+            // 
+            this.btnEn.Location = new System.Drawing.Point(12, 105);
+            this.btnEn.Name = "btnEn";
+            this.btnEn.Size = new System.Drawing.Size(75, 23);
+            this.btnEn.TabIndex = 2;
+            this.btnEn.Text = "1:-";
+            this.btnEn.UseVisualStyleBackColor = true;
+            this.btnEn.Click += new System.EventHandler(this.btnEn_Click);
+            // 
+            // btnFemtioore
+            // 
+            this.btnFemtioore.Location = new System.Drawing.Point(12, 134);
+            this.btnFemtioore.Name = "btnFemtioore";
+            this.btnFemtioore.Size = new System.Drawing.Size(75, 23);
+            this.btnFemtioore.TabIndex = 3;
+            this.btnFemtioore.Text = "50 öre";
+            this.btnFemtioore.UseVisualStyleBackColor = true;
+            this.btnFemtioore.Click += new System.EventHandler(this.btnFemtioore_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Växel:";
+            // 
+            // lblVaxel
+            // 
+            this.lblVaxel.AutoSize = true;
+            this.lblVaxel.Location = new System.Drawing.Point(52, 160);
+            this.lblVaxel.Name = "lblVaxel";
+            this.lblVaxel.Size = new System.Drawing.Size(0, 13);
+            this.lblVaxel.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 185);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 100);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnMocka
+            // 
+            this.btnMocka.Location = new System.Drawing.Point(143, 47);
+            this.btnMocka.Name = "btnMocka";
+            this.btnMocka.Size = new System.Drawing.Size(75, 23);
+            this.btnMocka.TabIndex = 9;
+            this.btnMocka.Text = "Mocka";
+            this.btnMocka.UseVisualStyleBackColor = true;
+            this.btnMocka.Click += new System.EventHandler(this.btnMocka_Click);
+            // 
+            // btnChocklad
+            // 
+            this.btnChocklad.Location = new System.Drawing.Point(143, 76);
+            this.btnChocklad.Name = "btnChocklad";
+            this.btnChocklad.Size = new System.Drawing.Size(75, 23);
+            this.btnChocklad.TabIndex = 10;
+            this.btnChocklad.Text = "Choklad";
+            this.btnChocklad.UseVisualStyleBackColor = true;
+            this.btnChocklad.Click += new System.EventHandler(this.btnChocklad_Click);
+            // 
+            // btnExpresso
+            // 
+            this.btnExpresso.Location = new System.Drawing.Point(143, 105);
+            this.btnExpresso.Name = "btnExpresso";
+            this.btnExpresso.Size = new System.Drawing.Size(75, 23);
+            this.btnExpresso.TabIndex = 11;
+            this.btnExpresso.Text = "Expresso";
+            this.btnExpresso.UseVisualStyleBackColor = true;
+            this.btnExpresso.Click += new System.EventHandler(this.btnExpresso_Click);
+            // 
+            // btnNyponsoppa
+            // 
+            this.btnNyponsoppa.Location = new System.Drawing.Point(143, 134);
+            this.btnNyponsoppa.Name = "btnNyponsoppa";
+            this.btnNyponsoppa.Size = new System.Drawing.Size(75, 23);
+            this.btnNyponsoppa.TabIndex = 12;
+            this.btnNyponsoppa.Text = "Nyponsoppa";
+            this.btnNyponsoppa.UseVisualStyleBackColor = true;
+            this.btnNyponsoppa.Click += new System.EventHandler(this.btnNyponsoppa_Click);
+            // 
+            // btnFruktsoppa
+            // 
+            this.btnFruktsoppa.Location = new System.Drawing.Point(143, 163);
+            this.btnFruktsoppa.Name = "btnFruktsoppa";
+            this.btnFruktsoppa.Size = new System.Drawing.Size(75, 23);
+            this.btnFruktsoppa.TabIndex = 13;
+            this.btnFruktsoppa.Text = "Fruktsoppa";
+            this.btnFruktsoppa.UseVisualStyleBackColor = true;
+            this.btnFruktsoppa.Click += new System.EventHandler(this.btnFruktsoppa_Click);
+            // 
+            // btnMotorolja
+            // 
+            this.btnMotorolja.Location = new System.Drawing.Point(143, 192);
+            this.btnMotorolja.Name = "btnMotorolja";
+            this.btnMotorolja.Size = new System.Drawing.Size(75, 23);
+            this.btnMotorolja.TabIndex = 14;
+            this.btnMotorolja.Text = "Motorolja";
+            this.btnMotorolja.UseVisualStyleBackColor = true;
+            this.btnMotorolja.Click += new System.EventHandler(this.Motorolja_Click);
+            // 
+            // btnKaffe
+            // 
+            this.btnKaffe.Location = new System.Drawing.Point(143, 221);
+            this.btnKaffe.Name = "btnKaffe";
+            this.btnKaffe.Size = new System.Drawing.Size(75, 23);
+            this.btnKaffe.TabIndex = 15;
+            this.btnKaffe.Text = "Kaffe";
+            this.btnKaffe.UseVisualStyleBackColor = true;
+            this.btnKaffe.Click += new System.EventHandler(this.btnKaffe_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "7:-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(224, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "8,50:-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(224, 110);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "12,50:-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(224, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "11:-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(224, 168);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "14:-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(224, 197);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "20:-";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(224, 226);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "6:-";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 23;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(35, 22);
+            this.toolStripLabel1.Text = "Reset";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 299);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnKaffe);
+            this.Controls.Add(this.btnMotorolja);
+            this.Controls.Add(this.btnFruktsoppa);
+            this.Controls.Add(this.btnNyponsoppa);
+            this.Controls.Add(this.btnExpresso);
+            this.Controls.Add(this.btnChocklad);
+            this.Controls.Add(this.btnMocka);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblVaxel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFemtioore);
+            this.Controls.Add(this.btnEn);
+            this.Controls.Add(this.btnFem);
+            this.Controls.Add(this.btnTio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTio;
+        private System.Windows.Forms.Button btnFem;
+        private System.Windows.Forms.Button btnEn;
+        private System.Windows.Forms.Button btnFemtioore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVaxel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnMocka;
+        private System.Windows.Forms.Button btnChocklad;
+        private System.Windows.Forms.Button btnExpresso;
+        private System.Windows.Forms.Button btnNyponsoppa;
+        private System.Windows.Forms.Button btnFruktsoppa;
+        private System.Windows.Forms.Button btnMotorolja;
+        private System.Windows.Forms.Button btnKaffe;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ Kaffeautomat2/Kaffeautomat.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kaffeautomat
+{
+    public partial class Form1 : Form
+    {
+        public static double
+        mocka = 7, chocklad = 8.5, expresso = 12.5, nyponsoppa = 11, fruktsoppa = 14, motorolja = 20, kaffe = 6,
+        tiokrona, femkrona, enkrona, femtioore, vaxel, betalning, bestallt = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        
+
+        private void btnTio_Click(object sender, EventArgs e)
+        {
+            tiokrona++;
+            betalning += 10;
+            KollaAllaPriser();
+        }
+
+        private void btnFem_Click(object sender, EventArgs e)
+        {
+            femkrona++;
+            betalning += 5;
+            KollaAllaPriser();
+        }
+
+        private void btnEn_Click(object sender, EventArgs e)
+        {
+            enkrona++;
+            betalning += 1;
+            KollaAllaPriser();
+        }
+
+        private void btnFemtioore_Click(object sender, EventArgs e)
+        {
+            femtioore++;
+            betalning += 0.5;
+            KollaAllaPriser();
+        }
+        public void ResetForm()
+        {
+            Form1.tiokrona = 0;
+            Form1.femkrona = 0;
+            Form1.enkrona = 0;
+            Form1.femtioore = 0;
+            Form1.betalning = 0;
+            Form1.vaxel = 0;
+            bestallt = 0;
+            lblVaxel.Text = "0";
+            KollaAllaPriser();
+            btnTio.Enabled = true;
+            btnFem.Enabled = true;
+            btnEn.Enabled = true;
+            btnFemtioore.Enabled = true;
+        }
+
+        private void KollaAllaPriser()
+        {
+
+            PrisKoll(betalning, Form1.mocka, btnMocka);
+            PrisKoll(betalning, Form1.chocklad, btnChocklad);
+            PrisKoll(betalning, Form1.expresso, btnExpresso);
+            PrisKoll(betalning, Form1.nyponsoppa, btnNyponsoppa);
+            PrisKoll(betalning, Form1.fruktsoppa, btnFruktsoppa);
+            PrisKoll(betalning, Form1.motorolja, btnMotorolja);
+            PrisKoll(betalning, Form1.kaffe, btnKaffe);
+        }
+        private void PrisKoll(double pengar, double pris, Button button)
+        {
+            if (pengar >= pris)
+            {
+                button.Enabled = true;
+            }
+            else
+            {
+                button.Enabled = false;
+            }
+        }
+
+        private void BeräknaVäxel(double betalat, double dryckpris)
+        {
+            vaxel = betalat - dryckpris;
+            lblVaxel.Text = vaxel.ToString();
+            btnTio.Enabled = false;
+            btnFem.Enabled = false;
+            btnEn.Enabled = false;
+            btnFemtioore.Enabled = false;
+            bestallt = 1;
+        }
+
+        private void btnMocka_Click(object sender, EventArgs e)
+        {
+            BeräknaVäxel(Form1.betalning, Form1.mocka);
+        }
+
+        private void btnChocklad_Click(object sender, EventArgs e)
+        {
+            BeräknaVäxel(Form1.betalning, Form1.chocklad);
+        }
+
+        private void btnExpresso_Click(object sender, EventArgs e)
+        {
+            BeräknaVäxel(Form1.betalning, Form1.expresso);
+        }
+
+        private void btnNyponsoppa_Click(object sender, EventArgs e)
+        {
+            BeräknaVäxel(Form1.betalning, Form1.nyponsoppa);
+        }
+
+        private void btnFruktsoppa_Click(object sender, EventArgs e)
+        {
+            BeräknaVäxel(Form1.betalning, Form1.fruktsoppa);
+        }
+
+        private void Motorolja_Click(object sender, EventArgs e)
+        {
+            BeräknaVäxel(Form1.betalning, Form1.motorolja);
+        }
+
+        private void btnKaffe_Click(object sender, EventArgs e)
+        {
+            BeräknaVäxel(Form1.betalning, Form1.kaffe);
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+    }
+}
